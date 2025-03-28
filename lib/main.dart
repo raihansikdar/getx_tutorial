37f7326927f4baba27c584786613321a25b34693
raihan sikdar <raihansikdar10@gmail.com>
2025-03-28 20:30:08 +0600

    counter app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:getx_tutorial/counter_app/counter_ui_screen.dart';
-import 'package:getx_tutorial/routing_home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,14 +10,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return MaterialApp(
       title: 'Getx Tutorial',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const RoutingHomePage(),
+      home: const CounterUiScreen(),
     );
   }
 }
